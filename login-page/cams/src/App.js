b03b4d284db052db9b1c579338a804d7e346a0c0
Nidhi Iyer <nidhiiyer240604@gmail.com>
2023-08-12 22:47:43 +0530

    Coded the basic css and js of the login page

---
+++ login-page/cams/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Username:', username);
+    console.log('Password:', password);
+    // You can implement your login logic here
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit} className="login-form">
+        <h2>Login</h2>
+        <div>
+          <label>Username:</label>
+          <input type="text" value={username} onChange={handleUsernameChange} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={handlePasswordChange} />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
+
