aac0824cb56b1bba98e49cb8a3895e764b90ac11
LakshmiAjay <102458649+LakshmiAjay@users.noreply.github.com>
2023-08-15 22:12:44 +0530

    Update Cards.js

--- login-page/cams/src/components/Cards.js
+++ login-page/cams/src/components/Cards.js
@@ -8,6 +8,7 @@
  * image, title, body text, and a button. The data for each Card is being mapped from the `test` array
  * and passed as props to the Card component.
  */
+
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
@@ -15,6 +16,16 @@ import './Card.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import img from 'D:\\Mentor\\Mentee-management-system\\login-page\\cams\\src\\components\\Images\\21STUCHH010173.jpg'
+/**
+ * The `Cards` function is a React component that renders a list of cards based on the `students` array
+ * and handles a click event on each card.
+ * @param props - The `props` parameter is an object that contains the properties passed to the `Cards`
+ * component. These properties include `enrnum`, `setenrnum`, `clicked`, `setclicked`, and `email`.
+ * @returns The Cards component is returning a JSX element that contains a Row component with multiple
+ * Card components inside it. The Card components display information such as title, imageUrl, and
+ * body.id. There is also a button inside each Card component that triggers the click function when
+ * clicked.
+ */
 function Cards(props) {
   let {enrnum,setenrnum,clicked,setclicked ,email } = props;
   
@@ -72,22 +83,7 @@ function click (e){
         </Col></>
       ))}
     </Row></>
-//     <Row xs={1} md={2} className="g-4">
-//   <CardGroup>
-//     {test.map(({ title, imageUrl, body})=>{
-//       return(      <Card className='student-card'>
-// <Card.Img variant="top" src={imageUrl} />
-// <Card.Body>
-//   <Card.Title>{title}</Card.Title>
-//   <Card.Text>{body}</Card.Text>
-//   <Button variant="primary">Go somewhere</Button>
-// </Card.Body>
-// </Card>)
-    
 
-
-// </CardGroup>
-// </Row>
   )
  
  
